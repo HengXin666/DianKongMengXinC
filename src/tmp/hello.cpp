25c7858cd961a00d631967b19c95493464b81d4f
Heng_Xin_666 <282000500@qq.com>
2024-09-25 16:46:00 +0800

    调整了语言占比

--- src/tmp/hello.cpp
+++ src/tmp/hello.cpp
@@ -1,27 +1,12 @@
-#pragma once
-/*
- * Copyright Heng_Xin. All rights reserved.
- *
- * @Author: Heng_Xin
- * @Date: 2024-09-25 14:07:04
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *	  https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/**
+ * https://github.com/HengXin666/HXNet
+ * 
+ * ぜひみてください!
  */
-#ifndef _HX_HELLO_H_
-#define _HX_HELLO_H_
 
 #include <iostream>
 
-bool _init_ = [] -> bool { std::cout << "Hello CPP!\n"; return false; } ();
-
-#endif // !_HX_HELLO_H_
+bool _init_ = [] -> bool { 
+    std::cout << "Hello CPP!\n";
+    return false; 
+}();
--- src/tmp/hello.jsp
+++ src/tmp/hello.jsp
@@ -1,13 +1,4 @@
 <body>
-    <p class="hx">Hello</p>
+    <p>Hello</p>
 </body>
-<script src="./js/jquery-3.7.1.js"></script>
-<script>
-    $(() => {
-        let dom = document.getElementsByClassName("hx")[0];
-        $(dom); // 这就是一个jQuery获取节点的对象
-
-        $("#text");
-        $("<a></a>"); // 这就是创建了元素节点
-    });
-</script>
+<script src="./js/jquery-3.7.1.js"/>
